1f1f79c1e51e9302e3b82289a2de5a7f3fa16da4
Auro Tripathy <ipserv@yahoo.com>
2018-10-14 16:07:25 -0700

    initial

---
+++ rccl/all_sum_unit_test.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow.contrib.nccl import all_sum
+
+with tf.device('/gpu:0'):
+    a = tf.get_variable(
+        f"a", initializer=tf.constant(1.0, shape=(2, 2)))
+
+with tf.device('/gpu:0'):
+    b = tf.get_variable(
+        f"b", initializer=tf.constant(2.0, shape=(2, 2)))
+
+sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                        log_device_placement=True))
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+with tf.device('/gpu:0'):
+    summed = sess.run(all_sum([a, b]))
+
+print(summed[0])
+print(summed[1])
