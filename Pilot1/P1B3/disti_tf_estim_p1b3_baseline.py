b5dd349f988bd6185b75c946713d6c4d2cfa49ed
Jeff Daily <jeff.daily@amd.com>
2018-11-27 19:21:14 +0000

    added print message for available GPUs

--- Pilot1/P1B3/disti_tf_estim_p1b3_baseline.py
+++ Pilot1/P1B3/disti_tf_estim_p1b3_baseline.py
@@ -10,6 +10,7 @@ from __future__ import division, print_function
 import numpy as np
 import tensorflow as tf
 import p1b3
+from tensorflow.python.client import device_lib
 
 # Model and Training parameters
 SEED = 2016
@@ -96,7 +97,14 @@ def fc_model_fn(features, labels, mode):
         mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
 def main():
+
+    print('Available GPUs', get_available_gpus())
+    
     tf.logging.set_verbosity(tf.logging.DEBUG)
     loader = p1b3.DataLoader(val_split=VAL_SPLIT,
                              test_cell_split=TEST_CELL_SPLIT,
