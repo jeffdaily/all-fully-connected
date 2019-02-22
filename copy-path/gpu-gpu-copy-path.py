7aafa0649513939a3515bc3c8797bd62425202eb
A. Tripathy <autrip@amd.com>
2019-02-22 03:25:18 +0000

    initial

---
+++ copy-path/gpu-gpu-copy-path.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import argparse
+import os
+import time
+
+"""
+run like this:
+python3 gpu-gpu-copy-path.py -d 10000
+"""
+
+parser = argparse.ArgumentParser(prog=os.path.basename(__file__),
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-d", "--dim", type=int, default=2,
+                    help="one side dimension of the square tensor")
+args = parser.parse_args()
+print('Dimensions chosen:', args.dim, 'X', args.dim)
+
+# var1 is on gpu:0 and initialized to 1
+with tf.device('/gpu:0'):
+    var0 = tf.get_variable(
+        "var1", initializer=tf.constant(1.0, shape=(args.dim, args.dim)))
+
+# var2 on gpu:1 and initialized to random values
+with tf.device('/gpu:1'):
+    var1 = tf.get_variable(
+        "var2", initializer=tf.random.normal(shape=(args.dim, args.dim)))
+
+# Define op to initilize 
+init_op = tf.initialize_all_variables()
+
+# Define graph to copy from gpu0 to gpu1
+assign_op = tf.assign(var1, var0)
+
+sess = tf.Session()
+sess.run(init_op)
+
+print("Before, var0")
+print(sess.run(var0))
+
+print("Before, var1")
+print(sess.run(var1))
+
+print("After, var1")
+start = time.clock()
+print(sess.run(assign_op))
+print('Time taken:', time.clock() - start)
